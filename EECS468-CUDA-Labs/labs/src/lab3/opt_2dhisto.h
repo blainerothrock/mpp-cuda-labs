207666ce40d12233670bd7772a300f0536d47c55
Nayan Mehta <nmh3011@alfred.ece.northwestern.edu>
2020-02-16 13:49:40 -0600

    Kernel Setup

---
+++ EECS468-CUDA-Labs/labs/src/lab3/opt_2dhisto.h
@@ -0,0 +1,9 @@
+#ifndef OPT_KERNEL
+#define OPT_KERNEL
+
+void opt_2dhisto( uint32_t *input[], size_t height, size_t width, uint8_t bins[HISTO_HEIGHT*HISTO_WIDTH], uint8_t HIST_HEIGHT, uint8_t HIST_WIDTH);
+
+/* Include below the function headers of any other functions that you implement */
+
+
+#endif
